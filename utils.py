21e53036c1fb8a02a2b12c9eb4d1aefef064302d
Hristo Boyadzhiev <boyadzhiev@win.tu-berlin.de>
2020-11-06 01:00:34 +0100

    move descriptor to utils

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 from sklearn.base import BaseEstimator
 import numpy as np
+import jax.ops
+
 
 class KRR(BaseEstimator):
     def __init__(self, sigma=1):
@@ -17,3 +19,23 @@ class KRR(BaseEstimator):
         data = np.load(f'models/{name}.npz')
         self.X = data['X']
         self.alphas = data['alphas']
+
+
+def fill_diagonal(a, value):
+    return jax.ops.index_update(a, np.diag_indices(a.shape[0]), value)
+
+
+def descriptor(x):
+    distances = np.sum((x[:, None] - x[None, :])**2, axis=-1)
+    distances = fill_diagonal(distances, 1) # because sqrt fails to compute gradient if called on 0s
+    distances = np.sqrt(distances)
+    D = 1 / distances
+    D = np.tril(D)
+    D = fill_diagonal(D, 0)
+    return D.flatten()
+
+
+def gaussian(x, x_, sigma=1):
+    d, d_ = descriptor(x), descriptor(x_)
+    sq_distance = np.sum((d - d_)**2)
+    return np.exp(-sq_distance / sigma)
