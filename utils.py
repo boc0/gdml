fix gaussian formula

utils.py
@@ -55,7 +55,7 @@ def descriptor(x):
 def gaussian(x, x_, sigma=1):
     d, d_ = descriptor(x), descriptor(x_)
     sq_distance = np.sum((d - d_)**2)
-    return np.exp(-sq_distance / sigma)
+    return 1 / np.sqrt(2 * np.pi * sigma**2) * np.exp(-sq_distance / sigma**2)
 
 def matrix_heatmap(matrix, **kwargs):
     ax = sns.heatmap(matrix, xticklabels=False, yticklabels=False, **kwargs)