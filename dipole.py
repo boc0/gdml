e0688dd2b17ad9d3f18347dd673b3f2f396e5252
Hristo Boyadzhiev <boyadzhiev@win.tu-berlin.de>
2020-11-23 14:30:14 +0100

    change normalization to overall, pick random data indices

--- dipole.py
+++ dipole.py
@@ -1,6 +1,7 @@
 from functools import partial
 
 import pandas as pd
+import numpy as onp
 
 import jax.numpy as np
 from jax import jit, vmap, jacfwd, jacrev
@@ -32,7 +33,7 @@ def symmetric(H):
     return sym
 
 @jit
-def kernel(x, x_, sigma=1):
+def kernel(x, x_, sigma=1.0):
     N, D = x.shape
 
     Dx, Dx_ = descriptor(x), descriptor(x_)
@@ -67,69 +68,22 @@ def kernel(x, x_, sigma=1):
     def delta_phi(q, k, p, l):
         return sigma**(-2) * ( D_difference @ delta_gamma(q, k, p, l) + gamma(q, k) @ gamma(p, l) )
     
-    def hess(q, k, p, l):
+    def hess(q, p, k, l):
         return -k_x * ( phi(q, k) * phi(p, l) - delta_phi(q, k, p, l) )
-
-    def derivatives(q, p, l):
-        vec_derivatives = vmap(partial(hess, q=q, p=p, l=l))
-        return vec_derivatives(np.arange(D))
-
-    def _derivatives(q, p):
-        # all_derivatives = vmap(partial(derivatives, q=q, p=p))
-        s = [derivatives(q=q, p=p, l=d) for d in np.arange(D)]
-        return np.sum(s)
     
-    @vmap
-    def kernel_elements(p):
-        vec_elements = vmap(partial(_derivatives, p=p))
-        return vec_elements(np.arange(N))
-    
-    # return kernel_elements(np.arange(N))
-
-    d = vmap(hess)
     rangeD = np.arange(D)
     rangeN = np.arange(N)
-    # d_ = d(rangeD, rangeN, rangeD, rangeN)
-    # print(d_)
 
-
-    def kernel_pq(p, q):
-        hess = lambda k, l: -k_x * ( phi(q, k) * phi(p, l) - delta_phi(q, k, p, l) )
-        # for k in range(N):
-        #     for l in range(N):
-        #         s += -k_x * ( phi(q, k) * phi(p, l) - delta_phi(q, k, p, l) )
-        sums = vmap(vmap(hess, (0, None)), (None, 0))(rangeN, rangeN)
-        # print(sums)
-        # print(np.sum(sums))
+    def kernel_pq(q, p):
+        _hess = lambda k, l: -k_x * ( phi(q, k) * phi(p, l) - delta_phi(q, k, p, l) )
+        _hess = partial(hess, q, p)
+        sums = vmap(vmap(_hess, (0, None)), (None, 0))(rangeN, rangeN)
         return np.sum(sums)
-
-
     
     K = vmap(vmap(kernel_pq, (0, None)), (None, 0))(rangeD, rangeD)
-
-
-
-    # print(K)
-    # print(K.shape)
+    K = (K + K.T) / 2
     return K
 
-    
-    def hess_ij(k, l):
-        rangeD = np.arange(D)
-        vhess = vmap(hess, (0, None, 0, None))(rangeD, k, rangeD, l)
-        vhess = vmap(partial(hess))
-        return vhess
-    
-    print(hess_ij(0, 1).shape)
-    print(hess_ij(0, 1))
-
-    rangeN = np.arange(N, dtype=np.int32)
-    mat = vmap(hess_ij, (0, 0))(range, range)
-    print(mat.shape)
-    return np.sum(mat, axis=(-1,-2))
-    
-
-from time import time
 
 
 @jit
@@ -143,21 +97,6 @@ def kernel_matrix(X, sigma=1):
     blocks = [list(x) for x in K]
     return np.block(blocks)
 
-'''
-    K = np.zeros((D, D))
-    K = []
-    for q in range(D):
-        K.append([])
-        for p in range(D):
-            s = 0
-            for k in range(N):
-                for l in range(N):
-                    s += -k_x * ( phi(q, k) * phi(p, l) - delta_phi(q, k, p, l) )
-            K[q].append(s)
-    K = np.array(K)
-    return K
-'''
-
 
 def unit(vector):
     return vector / np.linalg.norm(vector)
@@ -168,8 +107,8 @@ class VectorValuedKRR(KRR):
         self.X = X
         samples = X.shape[0]
         K = kernel_matrix(X, sigma=self.sigma)
-        self.means = np.mean(y, axis=0)
-        self.stdevs = np.std(y, axis=0)
+        self.means = np.mean(y)
+        self.stdevs = np.std(y)
         y = (y - self.means) / self.stdevs
         y = y.reshape(samples * 3)
         K = fill_diagonal(K, K.diagonal() + self.lamb)
@@ -196,37 +135,28 @@ class VectorValuedKRR(KRR):
             return -np.mean(error)
         angle = []
         for i in range(y.shape[0]):
-            angle.append( np.arccos(np.clip(unit(yhat[i]) @ unit(y[i]), -1.0, 1.0)) )
+            angle.append( np.degrees(np.arccos(np.clip(unit(yhat[i]) @ unit(y[i]), -1.0, 1.0))) )
         angle = np.array(angle)
         return np.mean(error), np.mean(angle)
 
-'''
-model = VectorValuedKRR(lamb=1e-5, sigma=0.5)
-for _ in range(10):
-    start = time()
-    model.fit(X[:5], y[:5])
-    print(time() - start, end=' ')
 
 
-print(model.predict(X[:2]))
-print(y[:2])
-
-print(model.predict(X[5:7]))
-print(y[5:7])
-
-'''
 sigma_choices = list(np.linspace(0.25, 2, 8))
 lambda_choices = [1e-5]#, 1e-4, 1e-3, 1e-2, 1]
 parameters = {'sigma': sigma_choices}#, 'lamb': lambda_choices}
 data_subset_sizes = np.linspace(10, 100, 10, dtype=int)
 test = slice(20000, 20100)
 errors, angles = [], []
+M = X.shape[0]
 
 from time import time
 
 for size in data_subset_sizes:
     start = time()
     print(f'size: {size}')
+
+    indices = onp.random.choice(M, size=size, replace=False)
+    Xtrain, ytrain = X[indices], y[indices]
 
     cross_validation = GridSearchCV(VectorValuedKRR(), parameters)
     cross_validation.fit(X[:size], y[:size])
