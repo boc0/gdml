cfbb6c89edfa711532c25ec87d45dee155903b96
Hristo Boyadzhiev <icoboyadzhiev@gmail.com>
2021-07-28 19:17:26 +0200

    repeat experiments and plot the average

--- experiment.py
+++ experiment.py
@@ -1,3 +1,4 @@
+from dipole import VectorValuedKRR, kernel_gauss, kernel_matern
 import os
 from time import time
 import numpy as onp
@@ -5,6 +6,7 @@ import jax.numpy as np
 import pandas as pd
 import mlflow
 import seaborn as sns
+from tqdm import tqdm
 from matplotlib import pyplot as plt
 from sklearn.experimental import enable_halving_search_cv  # noqa
 from sklearn.model_selection import HalvingGridSearchCV, GridSearchCV, RandomizedSearchCV
@@ -47,13 +49,13 @@ class Model:
             results = cv.cv_results_
             best = onp.argmin(results['mean_test_score'])
             best_params = results['params'][best]
-            print(f'{cls.__name__} best params: {best_params}')
+            # print(f'{cls.__name__} best params: {best_params}')
             mlflow.log_params(best_params)
             error = -cv.score(Xtest, ytest)
-            print(f'error: {error}')
+            # print(f'error: {error}')
             mlflow.log_metric('error', error)
             mlflow.log_metric('time', time() - start)
-        print(f'time taken: {time() - start}')
+        # print(f'time taken: {time() - start}')
         return error
 
 
@@ -63,8 +65,11 @@ EXPERIMENTS_FOLDER = 'experiments'
 
 class Experiment:
     shuffle = True
+    min_size = 10
     max_size = 100
+    test_size = 500
     n_subsets = 10
+    n_runs = 10
 
     @property
     def classes(self) -> list:
@@ -91,21 +96,40 @@ class Experiment:
         return os.path.join(EXPERIMENTS_FOLDER, self.name)
 
     def __init__(self):
+        if not self.multiple:
+            raise ValueError('Experiment should include multiple models!')
         mlflow.set_experiment(self.name)
         os.makedirs(self.folder, exist_ok=True)
 
+    def reduce(self):
+        try:
+            errs = self.errors
+        except AttributeError:
+            raise ValueError('Trying to reduce while errors not yet computed')
+        return {f'error {cls.description}': onp.mean(errs[cls.description], axis=0) for cls in self.classes}
+
     def plot(self):
+        errors = self.reduce()
+        errors |= {'samples trained on': self.sizes}
+
+        errors = pd.DataFrame(errors)
         cls1, cls2 = self.classes
-        data = self.errors
+        data = errors
         fig, ax = plt.subplots()
         desc1, desc2 = (cls.description for cls in (cls1, cls2))
-        sns.pointplot(x='samples trained on', y=f'error {desc1}', data=data, color='royalblue', label=desc1)
-        sns.pointplot(x='samples trained on', y=f'error {desc2}', data=data, color='orange', label=desc2)
+        sns.pointplot(x='samples trained on',
+                      y=f'error {desc1}', data=data, color='royalblue', label=desc1)
+        sns.pointplot(x='samples trained on',
+                      y=f'error {desc2}', data=data, color='orange', label=desc2)
         ax.legend(handles=ax.lines[::len(data)+1], labels=[cls1.__name__, cls2.__name__])
         ax.set_ylabel('error')
         plt.savefig(os.path.join(self.folder, 'learning_curve.png'))
         mlflow.log_figure(fig, 'learning_curve.png')
         # plt.show()
+
+    @property
+    def sizes(self):
+        return np.linspace(self.min_size, self.max_size, self.n_subsets, dtype=np.int32)
 
     def run(self):
         # prepare data
@@ -114,7 +138,7 @@ class Experiment:
         y = np.array(dataset['D'])
         M = X.shape[0]
 
-        test_indices = onp.random.choice(M, size=500, replace=False)
+        test_indices = onp.random.choice(M, size=self.test_size, replace=False)
         Xtest, ytest = X[test_indices], y[test_indices]
 
         # remove test samples from X, y
@@ -126,26 +150,45 @@ class Experiment:
             train_indices = onp.random.choice(M-self.max_size, size=self.max_size, replace=False)
             X, y = X[train_indices], y[train_indices]
 
-        data_subset_sizes = np.linspace(10, self.max_size, self.n_subsets, dtype=np.int32)
-        # errors = {cls.description: [] for cls in self.classes}
-        self.errors = {f'error {cls.description}': [] for cls in self.classes}
+        sizes = self.sizes
+        n_runs = self.n_runs
+        self.errors = {cls.description: onp.zeros((n_runs, len(sizes))) for cls in self.classes}
 
-        mlflow.start_run()
-        for size in data_subset_sizes:
-            Xtrain, ytrain = X[:size], y[:size]
+        with mlflow.start_run():
+            for i in tqdm(range(n_runs)):
+                with mlflow.start_run(nested=True):
+                    mlflow.log_param('run', i)
+                    for j, size in tqdm(list(enumerate(sizes))):
+                        Xtrain, ytrain = X[:size], y[:size]
+                        with mlflow.start_run(nested=True):
+                            # print(f'\nn_samples: {size}')
+                            mlflow.log_param('n_samples', size)
+                            for cls in self.classes:
+                                error = cls.train(Xtrain, ytrain, Xtest, ytest)
+                                self.errors[cls.description][i, j] = error
 
-            with mlflow.start_run(nested=True):
-                print(f'\nn_samples: {size}')
-                mlflow.log_param('n_samples', size)
-                for cls in self.classes:
-                    error = cls.train(Xtrain, ytrain, Xtest, ytest)
-                    self.errors[f'error {cls.description}'].append(error)
-
-        if not self.multiple:
-            return
-
-        self.errors |= {'samples trained on': data_subset_sizes}
-        self.errors = pd.DataFrame(self.errors)
         self.plot()
-        mlflow.end_run()
         return self.errors
+
+
+
+'''
+class Similarity(Experiment):
+    min_size = 5
+    max_size = 10
+    test_size = 1
+    n_subsets = 2
+
+    class Matern(VectorValuedKRR):
+        n_iter = 2
+        kernel = kernel_matern
+
+    class Gauss(VectorValuedKRR):
+        n_iter = 2
+        kernel = kernel_gauss
+
+
+
+ex = Similarity()
+ex.run()
+'''
